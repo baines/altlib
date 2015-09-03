8096e8bc744186f313f05a4e9339278ad868b66b
Alex Baines <alex@abaines.me.uk>
2015-09-03 18:19:29 +0100

    Various fixes to str_ref
    + find functions work with const + non-char types properly.
    + char* constructor changed to allow const char*'s to work while still
      making string literals bind to the char (&)[N] constructor.
    + pass_c_str now returns the result of the function passed in if
      appropriate.

--- str_common.h
+++ str_common.h
@@ -6,12 +6,14 @@ namespace alt {
 namespace detail {
 
 template<class T>
-T* str_find(const TStrRef<T>& needle, T* begin, T* end, int inc){
+T* str_find(const TStrRef<typename std::remove_const<T>::type>& needle, T* begin, T* end, int inc){
+	using U = typename std::remove_const<T>::type;
+
 	const size_t _sz = needle.size();
 	const T* _end = begin + _sz;
 	
 	while(_end != (end + inc)){
-		if(TStrRef<T>(begin, _end) == needle) return begin;
+		if(TStrRef<U>(begin, _end) == needle) return begin;
 		begin += inc;
 		_end += inc;
 	}
@@ -20,9 +22,9 @@ T* str_find(const TStrRef<T>& needle, T* begin, T* end, int inc){
 }
 
 template<class T>
-T* str_find_any(const TStrRef<T>& chars, T* begin, T* end, int inc){
-	for(char* p = begin; p != (end + inc); p += inc){
-		for(char c : chars){
+T* str_find_any(const TStrRef<typename std::remove_const<T>::type>& chars, T* begin, T* end, int inc){
+	for(T* p = begin; p != (end + inc); p += inc){
+		for(T c : chars){
 			if(*p == c) return p;
 		}
 	}
@@ -32,24 +34,24 @@ T* str_find_any(const TStrRef<T>& chars, T* begin, T* end, int inc){
 }
 
 template<class T>
-char* TStrRef<T>::find(const TStrRef<T>& needle, size_t offset){
-	return detail::str_find(needle, begin() + offset, end(), +1);
+const T* TStrRef<T>::find(const TStrRef<T>& needle, size_t offset) const {
+	return detail::str_find<const T>(needle, begin() + offset, end(), +1);
 }
 
 template<class T>
-char* TStrRef<T>::rfind(const TStrRef<T>& needle, size_t offset){
+const T* TStrRef<T>::rfind(const TStrRef<T>& needle, size_t offset) const {
 	const size_t _sz = needle.size();
-	return detail::str_find(needle, end() - _sz - offset, begin() + _sz, -1);
+	return detail::str_find<const T>(needle, end() - _sz - offset, begin() + _sz, -1);
 }
 
 template<class T>
-char* TStrRef<T>::find_any(const TStrRef<T>& chars, size_t offset){
-	return detail::str_find_any(chars, begin() + offset, end(), +1);
+const T* TStrRef<T>::find_any(const TStrRef<T>& chars, size_t offset) const {
+	return detail::str_find_any<const T>(chars, begin() + offset, end(), +1);
 }
 
 template<class T>
-char* TStrRef<T>::rfind_any(const TStrRef<T>& chars, size_t offset){
-	return detail::str_find_any(chars, end() - offset, begin(), -1);
+const T* TStrRef<T>::rfind_any(const TStrRef<T>& chars, size_t offset) const {
+	return detail::str_find_any<const T>(chars, end() - offset, begin(), -1);
 }
 
 }
--- str_ref.h
+++ str_ref.h
@@ -4,7 +4,7 @@
 #include <memory>
 
 namespace alt {
-
+namespace detail {
 template<class T>
 size_t StrLen(const T* ptr){
 	const T* p = ptr;
@@ -13,18 +13,39 @@ size_t StrLen(const T* ptr){
 }
 
 template<class T>
+using enable_if_result_void = typename std::enable_if<
+	std::is_same<void, typename std::result_of<T>::type>::value,
+	typename std::result_of<T>::type
+>;
+
+template<class T>
+using enable_if_result_not_void = typename std::enable_if<
+	!std::is_same<void, typename std::result_of<T>::type>::value,
+	typename std::result_of<T>::type
+>;
+
+}
+
+template<class T>
 struct TStrRef {
+
+	typedef T* iterator;
+	typedef const T* const_iterator;
+
 	TStrRef() = default;
 	
+	constexpr TStrRef(const T* p, size_t sz) : p(p), sz(sz){}
+	constexpr TStrRef(const T* a, const T* b) : p(a), sz(b - a){}
+	constexpr TStrRef(const T& c) : p(&c), sz(1){}
+
 	template<size_t N>
 	constexpr TStrRef(const T (&p)[N]) : p(p), sz(N-1){}
-	constexpr TStrRef(const T* p, size_t sz) : p(p), sz(sz){}
-	constexpr TStrRef(char* p) : p(p), sz(StrLen<T>(p)){}
-	constexpr TStrRef(const T* a, const T* b) : p(a), sz(b - a){}
-	constexpr TStrRef(const char& c) : p(&c), sz(1){}
+
+	template<class U>
+	constexpr TStrRef(const U* const& p) : p(p), sz(detail::StrLen<T>(p)){}
 
 	template<class F>
-	void pass_c_str(F&& func) const {
+	typename detail::enable_if_result_void<F(T*)>::type pass_c_str(F&& func) const {
 		const bool use_heap = sz >= 1024;
 		T* buf = use_heap ? new T[sz+1] : reinterpret_cast<T*>(alloca(sz+1));
 		memcpy(buf, p, sz * sizeof(T));
@@ -32,13 +53,40 @@ struct TStrRef {
 
 		func(buf);
 		
-		if(use_heap) delete [] buf;		
+		if(use_heap) delete [] buf;
 	}
 	
-	char* find(const TStrRef<T>& needle, size_t offset = 0);	
-	char* rfind(const TStrRef<T>& needle, size_t offset = 0);
-	char* find_any(const TStrRef<T>& chars, size_t offset = 0);
-	char* rfind_any(const TStrRef<T>& chars, size_t offset = 0);
+	template<class F>
+	typename detail::enable_if_result_not_void<F(T*)>::type pass_c_str(F&& func) const {
+		const bool use_heap = sz >= 1024;
+		T* buf = use_heap ? new T[sz+1] : reinterpret_cast<T*>(alloca(sz+1));
+		memcpy(buf, p, sz * sizeof(T));
+		buf[sz] = 0;
+
+		typename std::result_of<F(T*)>::type result = func(buf);
+		
+		if(use_heap) delete [] buf;
+		
+		return result;
+	}
+
+	const T& front() const { return p[0]; }
+	const T& back() const { return p[sz-1]; }
+	
+	const T* find(const TStrRef<T>& needle, size_t offset = 0) const;	
+	const T* rfind(const TStrRef<T>& needle, size_t offset = 0) const;
+	const T* find_any(const TStrRef<T>& chars, size_t offset = 0) const;
+	const T* rfind_any(const TStrRef<T>& chars, size_t offset = 0) const;
+
+	void remove_prefix(size_t n){
+		size_t min_sz = std::min(sz, n);
+		p += min_sz;
+		sz -= min_sz;	
+	}
+
+	void remove_suffix(size_t n){
+		sz = std::max<ssize_t>(0, sz - n);
+	}
 	
 	bool cmp(const TStrRef<T>& other) const {
 		return size() == other.size() && memcmp(p, other.data(), size() * sizeof(T)) == 0;
